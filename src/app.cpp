65e8d1b9f1bc4d2b6c0839e2c39a6086e7f4f49b
phil <phild@protonmail.com>
2021-06-08 21:54:21 +0200

    Add source files

---
+++ src/app.cpp
@@ -0,0 +1,10 @@
+#include "MyLibrary/MyLibrary.h"
+#include <iostream>
+using namespace pd;
+using namespace pd::MyLibrary;
+int main() {
+    MyAlgorithm myAlgorithm;
+    myAlgorithm.compute(1);
+
+std::cout << "hello"<< std::endl;
+}
